502086bb0c1b774cb11050d588bc9d18fa476ba1
Guilherme Murata <123330836+GuilhermeMurata@users.noreply.github.com>
2024-03-01 04:53:38 -0300

    Adicionando arquivos ao repositório

---
+++ src/calculoMedia.java
@@ -0,0 +1,35 @@
+
+public class calculoMedia {
+    public static void main(String[] args) {
+        System.out.println("****Média das notas****");
+        totalNotas = somaNotas();
+        mediaNotas(totalNotas);
+    }
+    public static double nota1 = 7.2;
+    public static double nota2 = 8.3;
+    public static double nota3 = 6.5;
+    public static double nota4 = 2.4;
+
+    // SOMA DE NOTAS
+    public static double somaNotas(){
+        double totalNotas = nota1 + nota2 + nota3 + nota4;
+        System.out.println("A soma das notas é:" + " " + totalNotas);
+        return totalNotas;
+    }
+
+    public static double totalNotas;
+
+    // MÉDIA DAS NOTAS + Condicional de aprovação
+    public static double mediaNotas(double totalNotas){
+        double notaFinal = totalNotas / 4;
+        System.out.println("A média das notas é:" + " " + notaFinal);
+
+        if (notaFinal >= 6){
+            System.out.println("Aluno aprovado");
+        }else{
+            System.out.println("Aluno reprovado");
+        }
+
+        return notaFinal;
+    }
+}
